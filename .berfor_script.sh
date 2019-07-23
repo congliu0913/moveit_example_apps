ce7d99d25de9ffec380a19757eb0d4efc27bae25
congliu0913 <congliu0913@126.com>
2019-07-23 11:06:46 +0800

    update moveit_ci

---
+++ .berfor_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export python_version=""
+
+export ROS_UNDERLAY="/home/robot/ws_moveit/devel"
+export CMAKE_ARGS="-DUSE_OPENVINO=ON -DBUILD_RANDOM_PICK=ON -DUSE_CAFFE=OFF"
+
+. /opt/intel/computer_vision_sdk/bin/setupvars.sh
