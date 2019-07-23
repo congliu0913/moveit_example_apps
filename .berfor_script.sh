58441053fc26a932e76262f37a1a67d3af08b8df
Cong Liu <congliu0913@126.com>
2019-07-23 14:22:52 +0800

    Update .berfor_script.sh

--- .berfor_script.sh
+++ .berfor_script.sh
@@ -6,3 +6,4 @@ export ROS_UNDERLAY="/home/robot/ws_moveit/devel"
 export CMAKE_ARGS="-DUSE_OPENVINO=ON -DBUILD_RANDOM_PICK=ON -DUSE_CAFFE=OFF"
 
 . /opt/intel/computer_vision_sdk/bin/setupvars.sh
+. /home/robot/ws_moveit/devel/setup.sh
